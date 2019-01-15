ea9a2e3965718195fc77c80b1d287880656ba62d
BillyPittman1993 <billytrading93@gmail.com>
2019-01-15 01:53:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const currentUser = 'Grace Hopper';
  
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = wlecomeMessage.upperCase();
+const excitedWelcomeMessage = wlecomeMessage.toUpperCase();
 
